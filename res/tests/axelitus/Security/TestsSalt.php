3dcf891d3d679d0d796c6a3cd1b8f2d2834f8b74
Axel Pardemann <axelitusdev@gmail.com>
2013-11-20 18:14:59 -0600

    Added the Salt tests file placeholder.

---
+++ res/tests/axelitus/Security/TestsSalt.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Part of composer package: axelitus/security
+ *
+ * @package     axelitus\Security
+ * @version     0.1
+ * @author      Axel Pardemann (axelitusdev@gmail.com)
+ * @license     MIT License
+ * @copyright   2013 - Axel Pardemann
+ * @link        http://axelitus.mx/projects/axelitus/security
+ */
+
+namespace axelitus\Security\Tests;
+
+/**
+ * Class TestsSalt
+ *
+ * @package axelitus\Security\Tests
+ */
+class TestsSalt extends TestCase
+{
+}
